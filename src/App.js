12c614b19cb4274154a3012010d4f4692e2b6463
Purva Goregaonkar <purvagoregaonkar008@gmail.com>
2022-01-30 19:40:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var a=10
+  return (
+    <div className="App">
+    <h1>Purva</h1>
+    </div>
+  );
+}
+
+export default App;
